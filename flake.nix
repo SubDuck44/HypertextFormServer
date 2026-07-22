8dffa3b71c52881791bddf681f25f4426627c939
Melinda <melinda.stobbe@mail.de>
2026-07-22 11:03:43 +0200

    init commit, innit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs.lib.fileset) toSource unions;
+        stdenv = pkgs.gccStdenv; # pkgs.clangStdenv;
+      in
+      rec {
+        packages.default = stdenv.mkDerivation {
+          pname = "form-server";
+          version = "0.0.1";
+
+          src = toSource {
+            root = ./.;
+            fileset = unions [
+              ./meson.build
+              ./src
+            ];
+          };
+
+          nativeBuildInputs = with pkgs; [
+            meson
+            ninja
+            pkg-config
+          ];
+
+          buildInputs = with pkgs; [
+          ];
+
+          preConfigure = ''
+            meson rewrite kwargs set project / version "$version"
+          '';
+
+          mesonBuildType = "release";
+          mesonFlags = [ "--werror" ];
+        };
+
+        devShells.default = (pkgs.mkShell.override {
+          inherit stdenv;
+        }) {
+          inputsFrom = [ packages.default ];
+          packages = with pkgs; [
+            clang-tools
+            just
+          ];
+        };
+      });
+}
