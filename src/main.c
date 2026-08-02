e06baa123a63d75281fd415d9589bc5700c6f039
Melinda <melinda.stobbe@mail.de>
2026-08-02 13:17:59 +0200

    add: timeout in libev

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <err.h>
+#include <ev.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,10 +30,15 @@
 #define DEBUG(x)
 #endif
 
-#define RESPONSE_ERR_405 "HTTP/1.0 405 METHOD NOT ALLOWED"
-#define RESPONSE_ERR_404 "HTTP/1.0 404 PAGE NOT FOUND"
-#define RESPONSE_OK "HTTP/1.0 200 OK DATA IN FLIGHT"
+#define RESPONSE_ERR_400 "HTTP/1.1 400 BAD REQUEST"
+#define RESPONSE_ERR_404 "HTTP/1.1 404 PAGE NOT FOUND"
+#define RESPONSE_ERR_405 "HTTP/1.1 405 METHOD NOT ALLOWED"
+#define RESPONSE_ERR_408 "HTTP/1.1 408 CONNECTION TIMEOUT"
+#define RESPONSE_ERR_505 "HTTP/1.1 505 HTTP VERSION UNSUPPORTED"
+#define RESPONSE_OK "HTTP/1.1 200 OK DATA IN FLIGHT"
+
 #define CONTENT_HTML "Content-Type: text/html; charset=utf-8"
+#define KEEP_ALIVE "Connection: close"
 
 const uint8_t CONGRATS[] = {
 #embed "../res/congrats.webp"
@@ -49,6 +55,14 @@ const uint8_t HOMEPAGE[] = {
 const uint8_t FINISHER[] = {
 #embed "finished.html"
 };
+
+////////////////////
+
+// Form replies will be dumped here
+#define FORM_DATA_PATH "out.html"
+FILE* form_data_file = NULL;
+
+int server_sock;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -75,6 +89,34 @@ const char* methodShow(Method meth) {
 	}
 #undef X
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef enum {
+	REQ_INVALID,
+	REQ_PARSE,
+	REQ_WRITE,
+	REQ_DONE,
+} ReadDirective;
+
+typedef struct {
+	ev_io         io;
+	ev_timer*     timer;
+	int           sock;
+	ReadDirective dir;
+
+	char*  buf;
+	size_t buf_len;
+	size_t buf_cap;
+	size_t buf_brk;
+
+	int content_length;
+} ReqWatcher;
+
+typedef struct {
+	ev_timer    timer;
+	ReqWatcher* req;
+} ReqTimeout;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -106,9 +148,15 @@ Response response_finisher = {
 	.body_len = sizeof(FINISHER),
 };
 
+Response response_400 = {0};
+
 Response response_404 = {0};
 
 Response response_405 = {0};
+
+Response response_408 = {0};
+
+Response response_505 = {0};
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -165,173 +213,272 @@ void sendAll(int socket, const uint8_t* data, size_t len) {
 	log("Done sending data!");
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 void sendHTTP(int socket, Response response) {
 	sendAll(socket, (uint8_t*) response.str, response.len);
 	if(response.body) sendAll(socket, response.body, response.body_len);
 }
 
-char* trimL(char* str) {
-	while(isspace(str[0])) {
-		str++;
-	}
+void killConnection(struct ev_loop* loop, ReqWatcher* watcher) {
+	close(watcher->sock);
 
-	return str;
+	ev_timer_stop(loop, watcher->timer);
+	free(watcher->timer);
+
+	ev_io_stop(loop, &watcher->io);
+	free(watcher->buf);
+	free(watcher);
 }
 
-char* trimR(char* str) {
-	size_t len = strlen(str);
+ReadDirective parseHTTP(ReqWatcher* req) {
+	log("\tparsing...");
 
-	while(len > 0 && isspace(str[len - 1])) {
-		len--;
+	Method meth    = METH_INVALID;
+	char*  path    = NULL;
+	char*  version = NULL;
+
+	char* header_key   = NULL;
+	char* header_value = NULL;
+
+	//////////////////// METHOD
+
+	char* token = strtok(req->buf, " ");
+	log("Method: %s", token);
+
+	if(strcmp("GET", token) == 0) {
+		meth = METH_GET;
+	} else if(strcmp("POST", token) == 0) {
+		meth = METH_POST;
+	} else {
+		sendHTTP(req->sock, response_405);
+		return REQ_DONE;
 	}
 
-	str[len] = 0;
+	//////////////////// PATH
 
-	return str;
-}
+	path = strtok(NULL, " ");
+	log("Path: %s", path);
 
-char* trim(char* str) {
-	return trimL(trimR(str));
-}
+	//////////////////// VERSION
 
-void handleRequest(int socket) {
-	FILE* f = fdopen(socket, "rb");
-	if(!f) die("Failed to upcast client socket");
+	version = strtok(NULL, "\n");
+	log("Version: %s", version);
 
-	static char*  buf = NULL;
-	static size_t cap = 0;
+	//////////////////// CONTENT LENGTH
 
-	Method meth           = METH_INVALID;
-	char   path[2048 + 1] = {0};
-	size_t content_length = 0;
-	bool   headers_done   = false;
+	if(meth == METH_POST) {
+		do {
+			header_key   = strtok(NULL, " ");
+			header_value = strtok(NULL, "\n");
+			log("Header: %s, Value: %s", header_key, header_value);
+		} while(strcasecmp("content-length:", header_key) != 0);
+		log("Content-Length: %s", header_value);
 
-	for(;;) {
-		ssize_t len = getline((char**) &buf, &cap, f);
-		if(len < 0) die("Could not get line from socket");
-
-		trimR(buf);
-
-		if(strlen(buf) == 0) break;
-
-		if(headers_done) continue;
-
-		if(meth == METH_INVALID) {
-			char* tok = strtok(buf, " ");
-			if(!tok) die("Could not get method token");
-
-#define X(x)                                                                   \
-	else if(strcmp(tok, #x) == 0) {                                            \
-		meth = METH_##x;                                                       \
-	}
-			if(0) {
-			} else if(strcmp(tok, "GET") == 0) {
-				meth = METH_GET;
-			} else if(strcmp(tok, "POST") == 0) {
-				meth = METH_POST;
-			} else {
-				die("Failed to parse signature line");
-			}
-
-			char* path_tok = strtok(NULL, " ");
-			if(!path_tok) die("Could not get path token");
-
-			strncpy(path, path_tok, sizeof(path) - 1);
-		} else {
-			char* key = strtok(buf, ":");
-			if(!key) die("Could not get header key");
-
-			key = trim(key);
-
-			if(strcasecmp(key, "content-length") == 0) {
-				char* val = strtok(NULL, "\n");
-				if(!val) die("Could not find value of content-length header");
-
-				char* val_end  = NULL;
-				content_length = strtoul(val, &val_end, 10);
-				if(*val_end != 0)
-					die("Could not convert content-length to int");
-
-				headers_done = true;
-			}
+		req->content_length = atoi(header_value);
+		if(req->content_length == 0) {
+			sendHTTP(req->sock, response_400);
+			return REQ_DONE;
 		}
 	}
 
-	log(
-		"Got request: %s %s %zu ", //
-		methodShow(meth),          //
-		path,                      //
-		content_length             //
-	);
+	//////////////////// ROUTE
 
-	bool ok = false;
+	if(strncmp("HTTP/1.1", version, 8) != 0) {
+		log("ERR: 505");
+		sendHTTP(req->sock, response_505);
+		return REQ_DONE;
+	}
 
 	switch(meth) {
 	case METH_GET:
-		if(strcasecmp(path, "/favicon.ico") == 0) {
-			log("Got favicon request, sending %zu bytes", sizeof(FAVICON));
-
-			sendHTTP(socket, response_favicon);
-			ok = true;
-		} else if(strcasecmp(path, "/congrats.webp") == 0) {
-			log("Got congrats request, sending %zu bytes", sizeof(CONGRATS));
-
-			sendHTTP(socket, response_congrats);
-			ok = true;
-		} else if(strcasecmp(path, "/") == 0) {
-			log("Got mainpage request, sending %zu bytes", sizeof(HOMEPAGE));
-
-			sendHTTP(socket, response_homepage);
-			ok = true;
-		} else if(strcasecmp(path, "/quit") == 0) {
-			log("Quit via request");
-
-			exit(0);
+		if(strcmp(path, "/") == 0) {
+			sendHTTP(req->sock, response_homepage);
+		} else if(strcmp(path, "/favicon.ico") == 0) {
+			sendHTTP(req->sock, response_favicon);
+		} else if(strcmp(path, "/congrats.webp") == 0) {
+			sendHTTP(req->sock, response_congrats);
+		} else {
+			sendHTTP(req->sock, response_404);
 		}
-
 		break;
 
 	case METH_POST:
-		if(strcasecmp(path, "/finished") == 0) {
-			log("Client finished form");
-
-			const char* res_path = "results.txt";
-			FILE*       res_file = fopen(res_path, "a");
-			if(!res_file) die("Could not open %s", res_path);
-
-			if(fread(buf, 1, content_length, f) < content_length)
-				die("Could not read %zu bytes from socket", content_length);
-
-			if(fwrite(buf, 1, content_length, res_file) < content_length)
-				die("Could not write %zu bytes to %s", content_length,
-				    res_path);
-
-			fclose(res_file);
-
-			sendHTTP(socket, response_finisher);
-
-			ok = true;
+		if(strcmp(path, "/finished") == 0) {
+			sendHTTP(req->sock, response_finisher);
+		} else {
+			sendHTTP(req->sock, response_404);
 		}
 		break;
 
 	default:
-		die("Unreachable");
+		die("Unreachable: Invalid method after validation");
 	}
 
-	if(!ok) { sendHTTP(socket, response_404); }
+	return REQ_DONE;
+}
 
-	log("Finished, closing connection...");
+#undef NEXT_TOKEN
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define CLEAR_BUFFER                                                           \
+	do {                                                                       \
+		req->buf_len = 0;                                                      \
+		req->buf_brk = 0;                                                      \
+	} while(0);
+
+void onReadReady(struct ev_loop* loop, ev_io* w, int revents) {
+	if(!(revents & EV_READ)) {
+		die("Unreachable: Didn't get EV_READ as revent on read-ready callback");
+	}
+
+	ReqWatcher* req = (ReqWatcher*) w;
+
+	ssize_t just_read = read(
+		req->sock,                  //
+		req->buf + req->buf_len,    //
+		req->buf_cap - req->buf_len //
+	);
+
+	log("INFO: Read %ld bytes from user", just_read);
+	// log("%.*s", (int) just_read, req->buf);
+
+	if(just_read < 0) {
+		close(req->sock);
+		die("ERR: Failed to read from client");
+	}
+
+	req->buf_len += just_read;
+
+	int newlines = 0;
+
+	for(size_t i = req->buf_len - just_read; i < req->buf_len; i++) {
+		if(req->buf[i] == '\n') {
+			newlines++;
+
+			if(newlines == 2) {
+				req->buf_brk = i + 1;
+
+				req->dir = parseHTTP(req);
+				log("Found newlines");
+				break;
+			}
+		} else if(req->buf[i] != '\r') {
+			newlines = 0;
+		}
+	}
+
+	switch(req->dir) {
+	case REQ_DONE:
+		log("Done!\n\n");
+
+		CLEAR_BUFFER;
+
+		killConnection(loop, req);
+
+		break;
+	case REQ_WRITE:
+		log("\t Writing...");
+
+		ssize_t just_written = fwrite(
+			req->buf,      //
+			sizeof(char),  //
+			req->buf_len,  //
+			form_data_file //
+		);
+
+		if(just_written < req->content_length) {
+			req->content_length -= just_written;
+		} else {
+			req->dir = REQ_PARSE;
+		}
+
+		CLEAR_BUFFER;
+
+		break;
+
+	default:
+		die("ERR: Got invalid read directive");
+
+		return;
+	}
+}
+
+#undef CLEAR_BUFFER
+
+void onTimeout(struct ev_loop* loop, ev_timer* t, int revents) {
+	log("WARN: Request timed out, closing connection");
+
+	if(!(revents & EV_TIMER)) {
+		die(
+			"ERR: Unreachable: Did not get EV_TIMER revent on timeout callback"
+		);
+	}
+
+	ReqTimeout* timer   = (ReqTimeout*) t;
+	ReqWatcher* watcher = timer->req;
+
+	sendHTTP(watcher->sock, response_408);
+
+	killConnection(loop, watcher);
+}
+
+void onNewConn(struct ev_loop* loop, ev_io* w, int revents) {
+	(void) w;
+
+	log("INFO: Got connection!");
+
+	if(!(revents & EV_READ)) {
+		die("Unreachable: Didn't get EV_READ as revent on read-ready callback");
+	}
+
+	ReqWatcher* watcher = malloc(sizeof(ReqWatcher));
+	ReqTimeout* timer   = malloc(sizeof(ReqTimeout));
+
+	if(!watcher) die("ERR: Failed to allocate memory for new read watcher");
+
+	*watcher = (ReqWatcher) {
+		.sock    = accept4(server_sock, NULL, NULL, SOCK_NONBLOCK),
+		.dir     = REQ_PARSE,
+		.timer   = (ev_timer*) timer,
+		.buf     = calloc(1 << 20, sizeof(char)),
+		.buf_len = 0,
+		.buf_cap = 1 << 20,
+	};
+
+	ev_io_init((ev_io*) watcher, onReadReady, watcher->sock, EV_READ);
+	ev_io_start(loop, (ev_io*) watcher);
+
+	////////////////////
+
+	*timer = (ReqTimeout) {
+		.req = watcher,
+	};
+
+#define TIMEOUT 10.0
+
+	ev_timer_init((ev_timer*) timer, onTimeout, TIMEOUT, 0.0);
+	ev_timer_start(loop, (ev_timer*) timer);
+
+	log("Started request handler for client with timeout: %f", TIMEOUT);
+
+#undef TIMEOUT
 }
 
 int main(void) {
+	// Generate HTTP responses
+
 	responseInit(&response_congrats);
 	headerAdd(RESPONSE_OK, &response_congrats);
+	headerAdd(KEEP_ALIVE, &response_congrats);
 	headerFinish(&response_congrats);
 
 	////////////////////
 
 	responseInit(&response_favicon);
 	headerAdd(RESPONSE_OK, &response_favicon);
+	headerAdd(KEEP_ALIVE, &response_favicon);
 	headerFinish(&response_favicon);
 
 	////////////////////
@@ -339,6 +486,7 @@ int main(void) {
 	responseInit(&response_homepage);
 	headerAdd(RESPONSE_OK, &response_homepage);
 	headerAdd(CONTENT_HTML, &response_homepage);
+	headerAdd(KEEP_ALIVE, &response_homepage);
 	headerFinish(&response_homepage);
 
 	////////////////////
@@ -346,21 +494,41 @@ int main(void) {
 	responseInit(&response_finisher);
 	headerAdd(RESPONSE_OK, &response_finisher);
 	headerAdd(CONTENT_HTML, &response_finisher);
+	headerAdd(KEEP_ALIVE, &response_finisher);
 	headerFinish(&response_finisher);
 
 	////////////////////
 
-	responseInit(&response_405);
-	headerAdd(RESPONSE_ERR_405, &response_405);
-	headerFinish(&response_405);
+	responseInit(&response_400);
+	headerAdd(RESPONSE_ERR_400, &response_400);
+	headerAdd(KEEP_ALIVE, &response_400);
+	headerFinish(&response_400);
 
 	////////////////////
 
 	responseInit(&response_404);
 	headerAdd(RESPONSE_ERR_404, &response_404);
+	headerAdd(KEEP_ALIVE, &response_404);
 	headerFinish(&response_404);
 
 	////////////////////
+
+	responseInit(&response_408);
+	headerAdd(RESPONSE_ERR_408, &response_408);
+	headerAdd(KEEP_ALIVE, &response_408);
+	headerFinish(&response_408);
+
+	////////////////////
+
+	responseInit(&response_505);
+	headerAdd(RESPONSE_ERR_505, &response_505);
+	headerAdd(KEEP_ALIVE, &response_505);
+	headerFinish(&response_505);
+
+	////////////////////
+	// Prepare form response outfile
+
+	form_data_file = fopen(FORM_DATA_PATH, "a");
 
 	struct sockaddr_in server_addr = {
 		.sin_family      = AF_INET,
@@ -368,10 +536,7 @@ int main(void) {
 		.sin_port        = htons(PORTNUM),
 	};
 
-	int server_sock;
-	int client_sock;
-
-	if((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	if((server_sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)) < 0) {
 		die("Failed to open socket");
 	}
 
@@ -388,18 +553,20 @@ int main(void) {
 
 	if(listen(server_sock, 1) < 0) die("Failed to listen on socket");
 
+	////////////////////
+	// Prepare event loop
+
+	struct ev_loop* event_loop = EV_DEFAULT;
+
+	ev_io conn_watcher = {0};
+	ev_io_init(&conn_watcher, onNewConn, server_sock, EV_READ);
+	ev_io_start(event_loop, &conn_watcher);
+
 	log(BOLD("Listening on port %d..."), PORTNUM);
 
-	for(;;) {
-		if((client_sock = accept(server_sock, (struct sockaddr*) NULL, NULL)) <
-		   0) {
-			die("Failed to accept connection from client");
-		}
+	int ev_fail = ev_run(event_loop, 0);
 
-		handleRequest(client_sock);
+	fclose(form_data_file);
 
-		close(client_sock);
-	}
-
-	return 0;
+	return ev_fail;
 }
